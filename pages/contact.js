9386ccd499df2a424e37cdc091d691494d7392f3
A-Sync <106713009+Hi-doki@users.noreply.github.com>
2022-08-29 21:59:31 +0100

    meowmeowmeow8

--- pages/contact.js
+++ pages/contact.js
@@ -44,7 +44,7 @@ export default function Home() {
 
       <div className="flex px-10 gap-5 py-5 place-content-center">
         <div className="flex transition ease-in-out duration-300 items-center place-content-center gap-2 p-2 py-2 rounded-md bg-opacity-0 bg-black hover:bg-opacity-50 hover:scale-110">
-          <image className="rounded-full" src="https://avatars.githubusercontent.com/u/106713009?v=4" width="50"/>
+          <img className="rounded-full" src="https://avatars.githubusercontent.com/u/106713009?v=4.png" alt="Kyatto PFP" width="50" height="50"/>
           <div>
             <p className="font-semibold">
               kyatto<span className="text-slate-500 text-sm">#2671</span>
@@ -52,7 +52,7 @@ export default function Home() {
           </div>
         </div>
         <div className="flex transition ease-in-out duration-300 text-semibold items-center place-content-center gap-2 p-2 py-2 rounded-md bg-opacity-0 bg-black hover:bg-opacity-50 hover:scale-110">
-          <image className="rounded-full" src="https://cdn.discordapp.com/avatars/500392354105131009/a_fe4fce34e7b66c72492bce68c31418a2.gif" width="50"/>
+          <img className="rounded-full" src="https://cdn.discordapp.com/avatars/500392354105131009/a_fe4fce34e7b66c72492bce68c31418a2.gif" alt="" width="50" height="50"/>
           <div>
             <p className="font-semibold">
               astromal<span className="text-slate-500 text-sm">#0001</span>
