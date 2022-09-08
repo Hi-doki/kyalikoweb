9fc759741b5644ad970553d7743371ff4d4a345a
A-Sync <106713009+Hi-doki@users.noreply.github.com>
2022-09-08 21:57:39 +0100

    meowmeowmeow25

---
+++ pages/astromal.js
@@ -0,0 +1,29 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+const astromal = () => {
+  return (
+    <div>
+        <div className="flex place-content-center text-center py-10 sm:p-48" >
+            <p className="transition ease-in-out rounded-md scale-75 p-5 font-bold text-5xl sm:text-7xl hover:bg-black hover:bg-opacity-20 hover:scale-100 duration-500">Astromal</p>
+        </div>
+
+        <div className="flex place-content-center p-10">
+            <div className=" text-center font-semibold text-xl sm:text-2xl w-3/4 bg-black bg-opacity-50 rounded-md p-10 ">
+                Astromal cool
+                <div className="p-2">
+                    uwu
+                </div>
+                <div>
+                    <span className="text-blue-200">meow :3</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+
+    
+  )
+}
+
+export default astromal
