a6085a8a517693fd19ed4b8c8626935bb2631870
A-Sync <106713009+Hi-doki@users.noreply.github.com>
2022-10-16 22:23:27 +0100

    meowmeowmeow36

--- pages/kyatto.js
+++ pages/kyatto.js
@@ -1,32 +1,59 @@
 import React from 'react'
 import Link from 'next/link'
+import Head from 'next/head'
 
 
 const kyatto = () => {
   return (
-    <div className="bg-white text-black selection:bg-[#00000050] ">
-        <div className="flex place-content-end relative p-5">
-            <Link href="https://kyaliko.com/">
-                <div>
-                    <button className="p-2 w-10 text-xl  bg-slate-100 rounded-md font-semibold after:content-['×']">
+    
+    <div className="bg-white text-black selection:bg-[#00000025] selection:text-cyan-100 uwu h-screen">
+        <Head>
+        <title>Kyatto - Kyaliko</title>
+        <meta name="Kyaliko" content="nya" />
+        <meta name="description" content="Kyaliko is a dev team that creates .NET Applications for people that are learning to code :3" />
+        <meta property="og:title" content="Kyatto - Kyaliko" />
+        <meta property="og:image" content="/PFP1.png" />
+        <meta property="og:description" content="Kyaliko is a dev team that creates .NET Applications for people that are learning to code :3" />
+        <link rel="icon" href="/kyafav.ico" />
+        </Head>
+        <div className="p-20 pt-40 bg-fixed backdrop-blur-lg static text-white h-screen">
 
-                    </button>
-                </div>
-            </Link>
-            
-        </div>
-        <div className="p-20 grid bg-fixed backdrop-blur-md ">
-            <div className="text-5xl font-bold">
+            <div className="transition ease-in-out duration-300 absolute top-2 right-2 hover:scale-110 selection:bg-white/0">
+                <Link href="https://kyaliko.com/">
+                   <button className="bg-white/20 p-2 rounded-md">
+                    <img className="stroke-white" src="close-120.svg" width="20px">
+                        
+                    </img>
+                   </button>
+                </Link>
+            </div>
+            <div className="text-6xl font-bold">
                 Kyatto
             </div>
-            <div className="text-xl pt-5 font-bold">
-                I create apps lol
+            <div className="text-2xl pt-5 font-semibold">
+                I created Kyaliko lol
+            </div>
+            <div className="pt-5 rounded-md">
+                <div className="text-xl grid bg-slate-50/25 rounded-md w-fit font-semibold">
+                    <span className="p-5">
+                        I create software in the .NET Framework coding language: C#
+                    </span>
+                    <span className="pt-0 p-5">
+                        I'm trying to make like 10 different projects all at once so my website looks terrible rn so yh
+                    </span>
+                </div>
             </div>
             
-            <div className="pt-5 font-semibold">
+            
+            <div className="pt-5 flex gap-5 font-semibold">
                 <Link href="https://github.com/kyaliko">
-                    <button className="transition ease-in-out duration-500 p-2 bg-pink-100 rounded-md hover:outline-violet-200 hover:outline-4 outline-offset-4">
+                    <button className="transition ease-in-out duration-500 p-2 bg-slate-50/25 rounded-md hover:bg-violet-200/25 hover:scale-110 hover:outline-4 outline-offset-4">
                         My GitHub
+                    </button>
+                </Link>
+                <Link href="https://discord.gg/GX6TVXafUm">
+                    <button className="transition ease-in-out duration-500 p-2 bg-slate-50/25 rounded-md hover:bg-violet-200/25 hover:scale-110 hover:outline-4 outline-offset-4">
+                        Discord Server
                     </button>
                 </Link>
             </div>
